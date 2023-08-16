534583d6b94b196a09aced48acfcf24d63f42522
shenaltissera <judeshenal.st@gmail.com>
2023-08-16 15:31:28 +0530

    clientside frontend implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+/**improt all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
+
+
+
+
 /** root routes */
 const router = createBrowserRouter([
   {
     path : '/',
-    element : <div>Root Route</div>
+    element : <Username></Username>
   },
   {
     path : '/register',
-    element : <div>Register Route</div>
+    element : <Register></Register>
+  },
+  {
+    path : '/password',
+    element : <Password></Password>
+  },
+  {
+    path : '/profile',
+    element : <Profile></Profile>
+  },
+  {
+    path : '/recovery',
+    element : <Recovery></Recovery>
+  },
+  {
+    path : '/reset',
+    element : <Reset></Reset>
+  },
+  {
+    path : '*',
+    element : <PageNotFound></PageNotFound>
   },
 
 ])
